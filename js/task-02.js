38dbe0079d9afc77ba3a52c3484c64c761c3b674
Danylo Kharkiv <donatelod12@gmail.com>
2022-12-11 19:22:05 +0200

    Do task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listEl = document.querySelector('#ingredients');
+
+const liEl = ingredients.map(ingredients => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredients;
+  return li;
+  console.dir(li);
+})
+
+
+listEl.append(...liEl);
